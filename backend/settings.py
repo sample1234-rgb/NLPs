989b36c5ef24c0e14d388978baf6ccf7fc1bb096
Gaurav Bhardwaj <61881543+sample1234-rgb@users.noreply.github.com>
2022-02-21 21:46:50 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -24,13 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4f^@t#i*(z@k3=y1e9ex3xv4q#%s4+xz_zy$mn=z9@u9421seg'
-
+SECRET_KEY = os.getenv('SECRET_KET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-
-
+ALLOWED_HOSTS= ['127.0.0.1:8001','localhost','nonu-lp-hope.herokuapp.com/']
 # Application definition
 
 INSTALLED_APPS = [
